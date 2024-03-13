3a6a4f4b8bc43d96bc6e8ef3e9a06ee631b68faa
jinnyjiinlee <163097586+jinnyjiinlee@users.noreply.github.com>
2024-03-13 22:42:43 +0900

    Add files via upload

---
+++ 나이 출력.js
@@ -0,0 +1,10 @@
+function solution(age) {
+    var result = 0;
+    result = 2022 - age;
+
+    let answer = 2022 - age + 1 ;
+    console.log("2022년 기준", age, "살이므로", result, "년생입니다." );
+
+    return answer;
+  
+}
