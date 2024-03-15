295bb803390014830664c5553c89cba7da3f8987
jinnyjiinlee <163097586+jinnyjiinlee@users.noreply.github.com>
2024-03-16 00:54:36 +0900

    Add files via upload

---
+++ 24.3.16(금)_몫구하기.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg949\cocoartf2759
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 function solution(num1, num2) \{\
+    var answer = 0;\
+    var answer = Math.floor( num1 / num2 );\
+ \
+    return answer;\
+    \
+    console.log("num1\uc0\u51060 ", num1 , "num2\u44032 ", num2, "\u51060 \u48064 \u47196 ", num1,"\u51012 ", num2, "\u47196  \u45208 \u45576  \u47787 ", answer,"\u51012  return \u54633 \u45768 \u45796 ." )\
+\}}
